834cd78ffabe96fff4d9d08f7ac9546420f17266
Maximiliano <djmaxirey@gmail.com>
2022-11-25 11:02:42 -0300

    script

---
+++ script.js
@@ -0,0 +1,12 @@
+let costoBase = 600
+let costoPremium = 1000
+let precioBase = 1200
+let precioPremium = 2000
+
+let gastosBase = prompt("¿Cuantos autos se lavaron hoy con el servicio base?")
+let gastosPremium = prompt("¿Cuantos autos se lavaron hoy con el servicio premium?")
+let gastoBase = (costoBase * gastosBase)
+let gastoPremium = (costoPremium * gastosPremium)
+let gananciaBase = ((precioBase * gastosBase) - costoBase)
+let gananciaPremium = ((precioPremium * gastosPremium) - costoPremium)
+alert("En el día de la fecha usted posee un costo operativo de " + gastoBase + " en el servicio de lavado basico, y un costo operativo de " + gastoPremium + " en el servicio de lavado premium. Usted genero " + gananciaBase + " de ganancia en el servicio base y " + gananciaPremium + "en el servicio premium")
