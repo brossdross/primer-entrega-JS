c2afec33bda82663e1caa0fe0bf622ba54f546d4
Maximiliano <djmaxirey@gmail.com>
2022-11-28 12:49:31 -0300

    agregado de if

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ let gastoPremium = (costoPremium * gastosPremium)
 let gananciaBase = (900 * gastosBase)
 let gananciaPremium = (1300 * gastosPremium)
 alert("En el día de la fecha usted posee un costo operativo de " + gastoBase + " en el servicio de lavado basico, y un costo operativo de " + gastoPremium + " en el servicio de lavado premium. Usted genero " + gananciaBase + " de ganancia en el servicio base y " + gananciaPremium + " en el servicio premium")
+
+if (gananciaBase > 5000 && gananciaPremium > 6000) {
+    return alert("Se cumplio el objetivo diario de lavados")
+}
+else if (gananciaBase < 5000 && gananciaPremium < 6000) {
+    return alert("No se cumplio el objetivo diario de lavados")
+}
+else if (gananciaBase > 5000 && gananciaPremium < 6000){
+    return alert("Se cumplio el objetivo diario de lavados base pero no de lavados premium")
+}
+else if (gananciaBase < 5000 && gananciaPremium > 6000){
+    return alert("se cumplio el objetivo diario de lavados premium pero no de lavados base")
+}
+else (gananciaBase === 0 && gananciaPremium === 0);{
+    return alert("El lavadero se encuentra cerrado")
+}
