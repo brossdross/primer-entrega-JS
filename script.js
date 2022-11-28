960f9898953cf0a2649ed455210c9a7a2e19bc41
Maximiliano <djmaxirey@gmail.com>
2022-11-28 12:30:19 -0300

    correcion de precios

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let costoBase = 600
 let costoPremium = 1000
-let precioBase = 1200
-let precioPremium = 2000
+let precioBase = 1500
+let precioPremium = 2300
 
 let gastosBase = prompt("¿Cuantos autos se lavaron hoy con el servicio base?")
 let gastosPremium = prompt("¿Cuantos autos se lavaron hoy con el servicio premium?")
 let gastoBase = (costoBase * gastosBase)
 let gastoPremium = (costoPremium * gastosPremium)
-let gananciaBase = (precioBase * gastosBase - costoBase)
-let gananciaPremium = (precioPremium * gastosPremium - costoPremium)
+let gananciaBase = (900 * gastosBase)
+let gananciaPremium = (1300 * gastosPremium)
 alert("En el día de la fecha usted posee un costo operativo de " + gastoBase + " en el servicio de lavado basico, y un costo operativo de " + gastoPremium + " en el servicio de lavado premium. Usted genero " + gananciaBase + " de ganancia en el servicio base y " + gananciaPremium + " en el servicio premium")
