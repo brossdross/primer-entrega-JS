2f6ad27182c8e22aceb6cf757e7207c92a623b1a
Maximiliano <djmaxirey@gmail.com>
2022-11-28 21:39:09 -0300

    prueba de correccion

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let costoPremium = 1000
 let precioBase = 1500
 let precioPremium = 2300
 
+let ciclo = 0
+
+while(ciclo == 0){
 let gastosBase = prompt("¿Cuantos autos se lavaron hoy con el servicio base?")
 let gastosPremium = prompt("¿Cuantos autos se lavaron hoy con el servicio premium?")
 let gastoBase = (costoBase * gastosBase)
@@ -11,18 +14,25 @@ let gananciaBase = (900 * gastosBase)
 let gananciaPremium = (1300 * gastosPremium)
 alert("En el día de la fecha usted posee un costo operativo de " + gastoBase + " en el servicio de lavado basico, y un costo operativo de " + gastoPremium + " en el servicio de lavado premium. Usted genero " + gananciaBase + " de ganancia en el servicio base y " + gananciaPremium + " en el servicio premium")
 
-if (gananciaBase > 5000 && gananciaPremium > 6000) {
-    return alert("Se cumplio el objetivo diario de lavados")
+if (gananciaBase === 0 && gananciaPremium === 0){
+    alert("El lavadero se encuentra cerrado1")
+    alert("la concha del error")
+    break
+}
+else if (gananciaBase > 5000 && gananciaPremium > 6000) {
+    alert("Se cumplio el objetivo diario de lavados")
+    break
 }
 else if (gananciaBase < 5000 && gananciaPremium < 6000) {
-    return alert("No se cumplio el objetivo diario de lavados")
+    alert("No se cumplio el objetivo diario de lavados")
+    break
 }
 else if (gananciaBase > 5000 && gananciaPremium < 6000){
-    return alert("Se cumplio el objetivo diario de lavados base pero no de lavados premium")
+    alert("Se cumplio el objetivo diario de lavados base pero no de lavados premium")
+    break
 }
 else if (gananciaBase < 5000 && gananciaPremium > 6000){
-    return alert("se cumplio el objetivo diario de lavados premium pero no de lavados base")
+    alert("se cumplio el objetivo diario de lavados premium pero no de lavados base")
+    break
 }
-else (gananciaBase === 0 && gananciaPremium === 0);{
-    return alert("El lavadero se encuentra cerrado")
 }
