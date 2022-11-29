f80ff5ca41dccaed74611f87f71b90ecd22febe8
Maximiliano <djmaxirey@gmail.com>
2022-11-28 22:09:11 -0300

    commit final

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ let gananciaPremium = (1300 * gastosPremium)
 alert("En el día de la fecha usted posee un costo operativo de " + gastoBase + " en el servicio de lavado basico, y un costo operativo de " + gastoPremium + " en el servicio de lavado premium. Usted genero " + gananciaBase + " de ganancia en el servicio base y " + gananciaPremium + " en el servicio premium")
 
 if (gananciaBase === 0 && gananciaPremium === 0){
-    alert("El lavadero se encuentra cerrado1")
-    alert("la concha del error")
+    alert("El lavadero se encuentra cerrado")
     
 }
 else if (gananciaBase > 5000 && gananciaPremium > 6000) {
@@ -35,5 +34,5 @@ else if (gananciaBase < 5000 && gananciaPremium > 6000){
     alert("se cumplio el objetivo diario de lavados premium pero no de lavados base")
     
 }
-let ciclo = prompt("Desea cargar otro dia de trabajo: SI = s ; NO = n")
+ciclo = prompt("Desea cargar otro dia de trabajo: SI = s ; NO = n");
 }
